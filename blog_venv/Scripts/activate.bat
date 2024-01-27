f6452fdb99e58c4a65f4159e7c4f78368f222a72
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-27 13:02:12 +0000

    new venv

--- blog_venv/Scripts/activate.bat
+++ blog_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Vaddi\python-practice\company-blog\blog-venv
+set VIRTUAL_ENV=C:\Users\Vaddi\python-practice\company-blog\blog_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(blog-venv) %PROMPT%
+set PROMPT=(blog_venv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(blog-venv) 
+set VIRTUAL_ENV_PROMPT=(blog_venv) 
 
 :END
 if defined _OLD_CODEPAGE (
