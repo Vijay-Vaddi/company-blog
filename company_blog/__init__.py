63a9a483d5d194dbb9f386894cc7af283f1090cb
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-28 15:41:18 +0000

    db set up

--- company_blog/__init__.py
+++ company_blog/__init__.py
@@ -1,6 +1,26 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 app = Flask(__name__)
+
+#######################################
+########## DATABASE SETUP #############
+#######################################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'blog.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+db.init_app(app)
+app.app_context().push()
+Migrate(app,db)
+
+#######################################
+########## BLUPRINT REGISTRATION ######
+#######################################
+
 
 from company_blog.core.views import core
 from company_blog.error_pages.handlers import error_pages
