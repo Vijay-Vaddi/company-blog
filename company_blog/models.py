a07d7614aae6b39ab12686ff37cca62bd0f380d0
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-29 17:23:08 +0000

    post db model set

--- company_blog/models.py
+++ company_blog/models.py
@@ -3,6 +3,7 @@
 from company_blog import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -19,7 +20,7 @@ class User(db.Model, UserMixin):
     profile_pic = db.Column(db.String(64), nullable = False, default='default_prof_pic.png')
     password_hash = db.Column(db.String(128))
 
-    post = db.relationship('BlogPost', backref = 'author', lazy = False)
+    posts = db.relationship('BlogPost', backref = 'author', lazy = False)
 
     def __init__(self, username, email, password) -> None:
         self.username = username
@@ -37,7 +38,29 @@ class User(db.Model, UserMixin):
 
 class BlogPost(db.Model):
 
-    pass
+    __tablename__ = 'blogposts'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(150), nullable=False )
+    text = db.Column(db.Text, nullable=False)    
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, user_id, title, text) -> None:
+        self.title = title
+        self.user_id = user_id
+        self.text = text
+    
+    
+    def __repr__(self) -> str:
+        return f"POST ID: {self.id} -- Date: {self.date} -- Title:{self.date}"
+
+
+
+
+    
 
     
 
