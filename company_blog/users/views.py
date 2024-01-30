a9e970a8cf6b23afc4d9fdef8850f6c8cafdb0ac
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-30 14:59:57 +0000

    login view

--- company_blog/users/views.py
+++ company_blog/users/views.py
@@ -24,14 +24,35 @@ def register():
         flash('Thanks for registering') 
         return redirect(url_for('users.login'))
     
-    return render_template('register.html')
-
-
-
-
-
+    return render_template('register.html', form=form)
 
 # login
+@users.route('/login', methods=["GET", "POST"])
+def login():
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash('Login success')
+            
+            next = request.args.get('next')
+
+            if next == None or not next[0]=='/':
+                next = url_for('core.index')
+
+            return redirect(next)
+
+    return render_template('login.html', form=form)            
+
+        
+
+
+
+
 # logout
 @users.route('/logout')
 def logout():
